966328ea5831f367a1bccc53066690d0372ad35e
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-10-28 12:22:54 +0100

    updated Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,11 +6,16 @@ const Hero = () => {
       <div className="max-w-[800px] mt-24 mx-auto h-screen w-full text-center flex flex-col" >
               <p className="text-[#00df9a] p-2 font-bold">GROWING WITH DATA ANALYTICS </p>
               <h1 className="font-bold md:text-7xl sm:text-6xl text-4xl md:py-6 ">GROW WITH DATA</h1>
-                <p className="p-4 md:text-5xl text-lg sm:text-4xl font-bold">Fast, flexible financing for </p>
+              <div className="flex items-center justify-between">
+                <p className="md:text-5xl text-xl sm:text-4xl font-bold py-4 ">Fast, flexible financing for </p>
 
-                <ReactTyped className="p-4 md:text-5xl text-lg sm:text-4xl font-bold"
+                <ReactTyped className="md:text-5xl text-lg sm:text-4xl font-bold pl-2"
                 strings={["BTC", "BTC", "SASS"]} typeSpeed={120} backSpeed={140} loop/>
-           </div> 
+              </div>
+             <p className="text-xl font-bold text-gray-500 md:2xl ">Monitor your data analytics to increase revenue for BTB, BTC, & SASS platform</p>
+             <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto p-3 ">Get Started</button>
+
+           </div>
 
  
         </div>
