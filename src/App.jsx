e50c2decdd113a5bb3a212773257844c2105921d
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-10-28 15:53:54 +0100

    Added Newsletter file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero'
 import Analytics from './Components/Analytics'
+import Newsletter from './Components/Newsletter'
 const App = () => {
   return (
     <> 
     <Navbar/>
         <Hero/>
         <Analytics/>
+        <Newsletter/>
     </>
 
 
