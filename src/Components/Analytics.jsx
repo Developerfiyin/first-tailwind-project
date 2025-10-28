2b13323cff0234b2c397de8d8d8737cff1626a16
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-10-28 21:06:56 +0100

    Updated Newsletter.jsx

--- src/Components/Analytics.jsx
+++ src/Components/Analytics.jsx
@@ -5,7 +5,7 @@ const Analytics = () => {
   return (
         <div className='w-full bg-white py-16 px-4'>
             <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-                 <img src={laptop} alt="/" className='w-600px mx-auto' />
+                 <img src={laptop} alt="Laptop image" className='w-600px mx-auto' />
                      <div className='flex flex-col justify-center'>
                   <p className='text-[#00df9a] font-bold'>DATA ANALYTICS DASHBOARDS</p>
                   <h1 className='md:text-4xl sm:text-3xl font-bold py-2'>Manage Data Analytics Centrally </h1>
@@ -16,11 +16,7 @@ const Analytics = () => {
                     quis accusantium soluta error animi aspernatur amet, adipisci velit?</p>
                   <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0  p-3 hover:bg-[#00df9a] hover:text-black'>Get Started</button>
                      </div>
-
-         
                    </div>
- 
-
                    </div>
   )
 }
