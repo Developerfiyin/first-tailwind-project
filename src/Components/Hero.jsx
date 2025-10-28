30d81f87b4753bca1669e76c5b32b269edfd41b6
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-10-28 09:21:12 +0100

    installed react-typed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,13 +1,18 @@
+import { ReactTyped } from "react-typed"
+
 const Hero = () => {
     return (
         <div className="text-white">
-      <div className="mt-[60px] max-w-[800px] h-screen w-full text-center flex flex-col" >
-              <p className="text-[#00df9a]">GROWING WITH DATA ANALYTICS </p>
-              <h1 className="text-white ">GROW WITH DATA</h1>
-                <p>Fast, flexible financing for </p>
-        </div> 
+      <div className="max-w-[800px] mt-24 mx-auto h-screen w-full text-center flex flex-col" >
+              <p className="text-[#00df9a] p-2 font-bold">GROWING WITH DATA ANALYTICS </p>
+              <h1 className="font-bold md:text-7xl sm:text-6xl text-4xl md:py-6 ">GROW WITH DATA</h1>
+                <p className="p-4 md:text-5xl text-lg sm:text-4xl font-bold">Fast, flexible financing for </p>
 
+                <ReactTyped className="p-4 md:text-5xl text-lg sm:text-4xl font-bold"
+                strings={["BTC", "BTC", "SASS"]} typeSpeed={120} backSpeed={140} loop/>
+           </div> 
 
+ 
         </div>
 
        
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <>
       <div className='text-white max-w-[1240px] flex items-center mx-auto p-4 justify-between'>
       <h1 className='text-[#00df9a] text-3xl font-bold'>REACT.</h1>
-         <ul className=' p-4 hidden md:flex'>
+         <ul className='hidden md:flex'>
             <li className='p-4'>Home</li> 
             <li className='p-4'>Company</li>
             <li className='p-4'>Resources </li>
@@ -22,7 +22,7 @@ const Navbar = () => {
             <div onClick={handleClick} className='block md:hidden ' > 
                  {!nav ? <AiOutlineMenu size={25}/> : <AiOutlineClose size={25}/> }</div>
 
-                     <div className={!nav ? ' fixed border-r h-full top-0 left-0 w-[60%] border-r-gray-900 bg-[#000300]' : 'fixed  -left-full'}>
+                     <div className={!nav ? 'fixed border-r h-full top-0 left-0 w-[60%] border-r-gray-900 bg-[#000300]' : 'fixed -left-full'}>
                       <h1 className='text-3xl text-[#00df9a]  font-bold mx-6'>REACT</h1>
                       <ul className='uppercase'>
                           <li className='p-4 border-b border-grey-700'>Home</li>
