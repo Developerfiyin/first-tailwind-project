eabc9dae5db8dfb69392c5c5338c1355d9ad2b36
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-10-28 12:59:44 +0100

    added images to assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero'
+import Analytics from './Components/Analytics'
 const App = () => {
   return (
     <> 
     <Navbar/>
         <Hero/>
+        <Analytics/>
     </>
 
 
---
+++ src/Components/Analytics.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Analytics = () => {
+  return (
+        <div className='w-full bg-white '>
+            <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>in</div>
+
+
+    </div>
+  )
+}
+
+export default Analytics
