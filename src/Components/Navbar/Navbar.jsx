ce529c4439b39da11740e6cd18b2ad73f9c41e96
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-10-29 18:11:24 +0100

    Found errors in Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
   const  [nav, setNav] = useState(false)
 
    const handleClick = () => {
-    setNav(!nav)
-   }
+    setNav(!nav);
+   };
 
   return (
     <>
@@ -19,22 +19,17 @@ const Navbar = () => {
             <li className='p-4'> About</li>
             <li className='p-4'>Contact </li>
            </ul>
-            <div onClick={handleClick} className='block md:hidden ' > 
-                 {!nav ? <AiOutlineMenu size={25}/> : <AiOutlineClose size={25}/> }</div>
+            <div onClick={handleClick} className='block md:hidden' > 
+                 {nav ? <AiOutlineMenu size={25}/> : <AiOutlineClose size={25}/> }</div>
 
-                     <div className={!nav ? 'fixed border-r h-full top-0 left-0 w-[60%] border-r-gray-900 bg-[#000300]' : 'fixed -left-full'}>
-                      <h1 className='text-3xl text-[#00df9a]  font-bold mx-6'>REACT</h1>
-                      <ul className='uppercase'>
+                     <ul className={nav ? 'fixed border-r h-full top-0 left-0 w-[60%] border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed -left-full'}>
+                      <h1 className='text-3xl text-[#00df9a] w-full font-bold mx-6'>REACT</h1>
                           <li className='p-4 border-b border-grey-700'>Home</li>
                           <li className='p-4 border-b border-grey-800'>Company</li>
                           <li className='p-4 border-b border-grey-800'>Resources </li>
                           <li className='p-4 border-b border-grey-700'> About</li>
                           <li className='p-4 border-b border-grey-700'>Contact </li>
-                      </ul>
-                      
-
-                        
-                       </div>
+                       </ul>
       </div>
 
     
