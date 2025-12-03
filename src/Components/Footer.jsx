265676bd929cdc2b0e6608fc744d13e2c15fd003
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-03 18:40:32 +0100

    Added a div tag for text

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { FaFacebookSquare } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div className='bg-black w-full px-4 py-16 '>
+    <div className='max-sm:w-[1240px]  px-4 py-16 text-gray-300 grid lg:grid-cols-3  '>
         
-        <div className='w-[1240px] flex flex-col '>
+        <div className='' >
+       <h1 className='text-[#00df9a] w-full text-3xl font-bold '>REACT</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+        necessitatibus id ad fugiat voluptate laudantium. Voluptate temporibus, cupiditate delectus quae minus aspernatur a liquid
+        libero obcaecati error minima reiciendis tenetur porro?</p>
+<div>
+  <FaFacebookSquare/>
+</div>
 
         </div>
         
         
-        Footer</div>
+        Footer </div>
   )
 }
 
